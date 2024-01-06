24a1d69ebe38f7651a216b61c7f1d695b0c1ce74
dojomojoboo <104776432+dojomojoboo@users.noreply.github.com>
2024-01-06 14:17:56 +0100

    Updated App.js added API code
    Added in the code to pull from the todos demo api from at jsonplaceholder website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: {} // Initialize todos as an object
+    }
+  }
+
+  API_URL = "https://jsonplaceholder.typicode.com/";
+
+  componentDidMount() {
+    this.refreshTeams();
+  }
+
+  async refreshTeams() {
+    fetch(this.API_URL + "todos/1")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ todos: data })
+      })
+  }
+
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>This is the web api data from the demo site https://jsonplaceholder.typicode.com/<hr></hr>
+            <b>{todos.title}</b>{todos.id}
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
